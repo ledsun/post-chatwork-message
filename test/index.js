08404cdc7114328951b44909e31010e758ab98ff
ledsun <shigeru.nakajima@gmail.com>
2015-01-11 23:29:30 +0900

    Write a test code in ES6.

--- test/index.js
+++ test/index.js
@@ -2,8 +2,8 @@ var token = require('../token'),
     postChatworkMessage = require('../lib'),
     assert = require('assert')
 
-describe('postChatworkMessage', function() {
-    it('do without error', function() {
+describe('postChatworkMessage', () => {
+    it('do without error', () => {
         return postChatworkMessage(token.token, token.roomId, 'A test messago for post-chatwork-message')
             .then(function(body) {
                 assert(body)
