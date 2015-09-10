28b5724cc9d3686c95652856b94e94b0e762be69
ledsun <shigeru.nakajima@gmail.com>
2015-09-10 23:27:33 +0900

    Refactor test Fix name of a variable. Fix indent.

--- test/index.js
+++ test/index.js
@@ -3,10 +3,10 @@ import postChatworkMessage from '../lib'
 import assert from 'assert'
 
 describe('postChatworkMessage', () => {
-    it('do without error', () => {
-        return postChatworkMessage(token.token, token.roomId, 'A test messago for post-chatwork-message')
-            .then(function(body) {
-                assert(body)
-            })
-    })
+  it('do without error', () => {
+    return postChatworkMessage(token.token, token.roomId, 'A test messago for post-chatwork-message')
+      .then(function(res) {
+        assert(res)
+      })
+  })
 })
