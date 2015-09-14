ec57b35088614b2e2551a8abfcf5038c98b73bff
ledsun <shigeru.nakajima@gmail.com>
2015-09-14 09:48:36 +0900

    Empower test - Use the power-assert. - Use the espower-babel. - Add a assertion.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,14 @@
 import token from '../token'
-import postChatworkMessage from '../lib'
-import assert from 'assert'
+import postChatworkMessage from '../src'
+import assert from 'power-assert'
 
 describe('postChatworkMessage', () => {
   it('do without error', () => {
     return postChatworkMessage(token.token, token.roomId, 'A test messago for post-chatwork-message')
       .then(function(res) {
-        assert(res)
+        assert(res, 'The res is not null.')
+        assert.equal(res.status, 200)
+        assert(res.body.message_id, 'The message_id is not null.')
       })
   })
 })
